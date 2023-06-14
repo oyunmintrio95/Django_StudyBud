4d4a47cc3c3ea45f28ea69ad276f78d1fde79b1d
oyunmintrio95 <oyunmintrio95@gmail.com>
2023-06-13 22:31:59 -0700

    static files

--- base/views.py
+++ base/views.py
@@ -103,6 +103,17 @@ def room(request,pk):
                'participants': participants}
     return render(request, 'base/room.html', context)
 
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    #we can get all the children of specific model by using modelname_set
+    rooms = user.room_set.all()
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'user': user, 'rooms': rooms,
+               'room_messages':room_messages, 'topics':topics}
+
+    return render(request, 'base/profile.html', context)
+
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
@@ -110,7 +121,9 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit=False)
+            room.host = request.user
+            room.save()
             return redirect('home')
 
     context = {'form': form}
